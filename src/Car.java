5f3ee8d024445edb06f730df55b601e0c4949f06
Kommit <aksaghovafatima@gmail.com>
2023-01-12 23:44:57 +0300

    Проверьте-ДЗ

--- src/Car.java
+++ src/Car.java
@@ -1,4 +1,54 @@
+public class Car {
 
+    String brand; // Бренд
+    String model; // Модель
+    String color; // Цвет
+    String country; // Строна сборки
+    double engineVolume; // Объем двигателя в литрах
+    int year; // Год производства
+
+
+    public Car(String brand, String color, String model, String country, double engineVolume, int year) {
+
+        if (brand == null || brand.isBlank()) {
+            this.brand = "Автомобиль ";
+        } else {
+            this.brand = brand;
+        }
+        if (model == null || model.isBlank()) {
+            this.model = "";
+        } else {
+            this.model = model;
+        }
+        if (engineVolume <= 0) {
+            this.engineVolume = 1.5;
+        } else {
+            this.engineVolume = engineVolume;
+        }
+        if (color == null || color.isBlank()) {
+            this.color = "Белый";
+        } else {
+            this.color = color;
+        }
+
+        if (year <= 0) {
+            this.year = 2000;
+        } else {
+            this.year = year;
+        }
+
+        if (country == null || country.isBlank()) {
+            this.country = "Made in ...";
+        } else {
+            this.country = country;
+        }
+    }
+
+    @Override
+    public String toString() {
+        return "Car{" + "Бренд ='" + brand + '\'' + ", Модель ='" + model + '\'' + ",  Цвет ='" + color + '\'' + ",  Страна ='" + country + '\'' + ", Обьем двиголеля =" + engineVolume + ",  Год =" + year + '}';
+    }
+}
 
 
 
--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,21 @@
 public class Main {
     public static void main(String[] args) {
 
-        System.out.println(" привет ");
+        String brand; // Бренд
+        String model; // Модель
+        String color; // Цвет
+        String country; // Строна сборки
+        double engineVolume; // Объем двигателя в литрах
+        int year; // Год производства
+
+        Car car1 = new Car( "Lada ", "Чёрный" ,"Granta" ,  "Япония", 1.5, 2015 );
+        Car car2 = new Car("Audi", "Чёрный", "A8 50L TDI quattro", "Германия", 3.0, 2020);
+        Car car3 = new Car( "BMW",  "Чёрный", "Z8", "Германия", 3.0, 2021);
+        Car car4 = new Car( "Kia","Красный" , "Sportage IV", "Южная Корея", 2.4, 2018);
+        Car car5 = new Car("Hyundai", "Оранжевый", "Avante",  "Южная Корея", 1.6,2016);
+
+
+        System.out.println(car1 + "\n" + car2 + '\n' + car3 + '\n' + car4 + '\n' + car5);
     }
 
 }
